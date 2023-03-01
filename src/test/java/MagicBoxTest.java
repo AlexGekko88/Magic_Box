c06667be258036f532b842ef613942b068689519
AlexGekko88 <115148133+AlexGekko88@users.noreply.github.com>
2023-03-01 15:28:26 +0300

    Add

---
+++ src/test/java/MagicBoxTest.java
@@ -0,0 +1,63 @@
+import org.example.MagicBox;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class MagicBoxTest {
+
+    static MagicBox<String> boxString = new MagicBox<>("boxString", 2);
+    static MagicBox<Integer> boxInt;
+    static List<Integer> list = Arrays.asList(11, 49, 8, -9, 74, 5);
+
+    @BeforeAll
+    static void setUp() {
+        boxInt = new MagicBox<>("boxInt", list.size());
+    }
+
+
+    @DisplayName("Тестирование метода boolean add()")
+    @ParameterizedTest
+    @MethodSource("getArguments")
+    void addTest(String item, boolean isAdded) {
+        Assertions.assertEquals(isAdded, boxString.add(item));
+    }
+
+    private static Stream<Arguments> getArguments() {
+        return Stream.of(
+                Arguments.of("один", true),
+                Arguments.of("два", true),
+                Arguments.of("три", false)
+        );
+    }
+
+
+    @DisplayName("Тестирование метода T pick()")
+    @Test
+    void pickTest() {
+
+        for (Integer i : list) {
+            boxInt.add(i);
+        }
+
+        Set<Integer> set = new HashSet<Integer>(list);
+        Assertions.assertTrue(set.contains(boxInt.pick()));
+    }
+
+
+    @DisplayName("Тестирование метода T pick() на выброс исключения")
+    @Test
+    void pickTestEx() {
+        boxInt.add(list.get(1));
+        Assertions.assertThrows(RuntimeException.class, () -> boxInt.pick());
+    }
+}
